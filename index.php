03294bea38906b73d5bdb2b0c360ec6e45d56e09
DieniferM <dienifer92@hotmail.com>
2024-09-24 09:44:32 -0300

    estrutura quase pronta

--- index.php
+++ index.php
@@ -3,20 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assessoria de Enfermagem</title>
+    <title>Vânia Massoterapeuta</title>
     <link rel="stylesheet" type="text/css" href="<?php echo 'styles.css?v=' . filemtime('styles.css'); ?>"/>
 
 </head>
 <body>
     <header>
-        <div class="logo">#Logo - frase</div>
+        <!-- <div class="logo">#Logo</div> -->
         <nav>
             <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#sobre">Sobre</a></li>
-                <li><a href="#entenda-etapas">Entenda as Etapas</a></li>
-                <li><a href="#direitos-italia">Quem Tem Direito</a></li>
-                <li><a href="#documentos">Documentos</a></li>
+                <li><a href="#home">Inicio</a></li>
+                <li><a href="#entenda-etapas">Benefícios</a></li>
+                <li><a href="#sobre">Quem sou eu</a></li>
+                <li><a href="#servicos">Serviços</a></li>
+                <!-- <li><a href="#documentos">Documentos</a></li> -->
                 <li><a href="#contato">Contato</a></li>
             </ul>
         </nav>
@@ -24,26 +24,26 @@
 
     <section id="home">
         <div class="home-content">
-            <h1>Bem-vindo à Assessoria de Enfermagem</h1>
+            <h1>Conheça os benefícios</h1>
         </div>
     </section>
 
    <section id="sobre">
     <div class="sobre-content">
-        <h2>Sobre Nós</h2>
-        <p>Nossa missão, valores e objetivos guiam nosso trabalho como assessoria de enfermagem.</p>
+        <h2>Minha Trajetória na Massoterapia</h2>
+        <p>Descrição completa1</p>
         <div class="blocos">
             <div class="bloco">
-                <h3>Missão</h3>
-                <p>Descrição breve sobre a missão.</p>
+                <h3>Exemplo1</h3>
+                <p>Descrição1.</p>
             </div>
             <div class="bloco">
-                <h3>Valores</h3>
-                <p>Descrição breve sobre os valores.</p>
+                <h3>Exemplo2</h3>
+                <p>Descrição2.</p>
             </div>
             <div class="bloco">
-                <h3>Objetivo</h3>
-                <p>Descrição breve sobre o objetivo.</p>
+                <h3>Exemplo13</h3>
+                <p>Descrição13.</p>
             </div>
         </div>
     </div>
@@ -53,47 +53,55 @@
    <section id="entenda-etapas">
     <div class="entenda-content">
         <div class="entenda-texto">
-            <h2>Entenda as Etapas</h2>
+            <h2>Como a massoterapia pode melhorar sua saúde e bem-estar?</h2>
             <ul>
-                <li>Registro na Ordem dos Enfermeiros</li>
-                <li>Tradução dos documentos</li>
-                <li>Validação de certificados</li>
-                <li>Preparação para o exame de equivalência</li>
-                <li>Solicitação de visto</li>
+                <li>Alívio do Estresse</li>
+                <li>Redução de Dores Musculares</li>
+                <li>Melhora da Circulação Sanguínea e Linfática</li>
+                <li>Aumento da Flexibilidade</li>
+                <li>Fortalecimento do Sistema Imunológico</li>
+                <li>Melhora da Qualidade do Sono</li>
+                <li>Melhoria da Postura</li>
+                <li>Alívio de Dores de Cabeça e Enxaquecas</li>
             </ul>
         </div>
         <div class="entenda-imagem">
-            <img src="https://images.unsplash.com/photo-1696952384801-079962407b0d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Entenda as Etapas">
+            <img src="img/img1.jpg" alt="Entenda as Etapas">
         </div>
     </div>
 </section>
 
-    <section id="direitos-italia">
-        <h2>Quem Tem Direito a Trabalhar como Enfermeiro na Itália</h2>
+    <section id="servicos">
+        <h2>Serviços</h2>
         <div class="direitos-content">
             <div class="col">
-                <h3>Requisitos</h3>
+                <h3>Massagem terapêutica</h3>
                 <ul>
-                    <li>Diploma Reconhecido</li>
-                    <li>Experiência Profissional</li>
-                    <li>Certificação Linguística</li>
+                    <li>Aliviar dores musculares e articulares</li>
+                    <li>Melhorar a circulação</li>
+                    <li>Aumentar a mobilidade e flexibilidade</li>
+                    <li>Alívio do estresse físico e mental</li>
+                    <li>Tratar lesões esportivas</li>
                 </ul>
             </div>
             <div class="col">
-                <h3>Outros</h3>
+                <h3>Drenagem linfática</h3>
                 <ul>
-                    <li>Requisitos de Visto</li>
-                    <li>Registro no Conselho de Enfermagem</li>
+                    <li>Redução de Inchaço e Retenção de Líquidos</li>
+                    <li>Aceleração da Recuperação Pós-operatória</li>
+                    <li>Fortalecimento do Sistema Imunológico</li>
+                    <li>Combate à Celulite</li>
+                    <li>Melhora da Circulação e Desintoxicação</li>
                 </ul>
             </div>
         </div>
     </section>
 
-    <section id="documentos">
+    <!-- <section id="documentos">
         <h2>Ainda não encontrou todos os documentos?</h2>
         <p>Nós encontramos para você!</p>
         <p>Se você está tendo dificuldades para reunir todos os documentos necessários para trabalhar como enfermeiro na Itália, nossa equipe pode ajudar. Entre em contato conosco para obter suporte completo na busca e obtenção dos documentos.</p>
-    </section>
+    </section> -->
 
     <section id="contato">
         <h2>Contato</h2>
@@ -114,10 +122,13 @@
     <footer>
         <div class="footer-info">
             <p>Telefone: (39) 1234-5678</p>
-            <p>Endereço: Exemplo, São Paulo, SP</p>
-            <p>&copy; 2024 Assessoria de Enfermagem. Desenvolvido por https://dienifermendonca.tech/</p>
+            <p>Endereço: Exemplo, Santa Catarina, SC</p>
         </div>
-        <button id="login-btn">Login</button>
+        <div class="footer-copy">
+            <p>&copy; Desenvolvido por dienifermendonca.tech © 2024. Todos os direitos reservados.</p>
+        </div>
+
+        <!-- <button id="login-btn">Login</button> -->
     </footer>
 
     <script src="scripts.js"></script>
